acd8c9f38f879ed8903602b1e16bcad828765ce8
nguyen huy nam <huynam67k1@gmail.com>
2022-02-20 14:48:47 +0700

    first commit

---
+++ TH_Tuan1/app/src/main/java/com/example/bt_th_tuan1/BTTH1.kt
@@ -0,0 +1,11 @@
+package com.example.bt_th_tuan1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BTTH1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.`btth1.hn.xml`)
+    }
+}
